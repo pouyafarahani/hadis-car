e68e3be85a775f25158ff8a3f27c109f09f36933
pouyafarahani <105992449+pouyafarahani@users.noreply.github.com>
2023-01-12 20:35:20 +0330

    create file requriments.txt and finish page 1 and 2

--- home/views.py
+++ home/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from django.contrib import messages
 
+import requests
+
 from .forms import CarForms
+
+url = "https://driver-vehicle-licensing.api.gov.uk/vehicle-enquiry/v1/vehicles"
+payload = "{\n\t\"registrationNumber\": \"Sm18ebp\"\n}"
+headers = {
+                'x-api-key': 'LkmZehCC9z96eAkRtthdQ5fD0IAWsLyH9fQrJHSb',
+                'Content-Type': 'application/json'
+            }
 
 
 def HomeView(request):
@@ -9,7 +18,9 @@ def HomeView(request):
         form = CarForms(request.POST)
         if form.is_valid():
             form.save()
-            return render(request, 'detail_car.html')
+            response = requests.request("POST", url, headers=headers, data=payload)
+            res = response.json()
+            return render(request, 'detail_car.html', {'response': res})
 
         # not valid
         messages.warning(request, 'Please enter your information correctly')
